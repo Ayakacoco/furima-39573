b942a5766b8aa6641aff9c37583a3f361c336c2d
Ayakacoco <ayakacoco28@gmail.com>
2023-08-15 13:36:08 +0900

    モデル作成とActive Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,11 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:product, :image, :item_name, :category_id, :sales_status_id, :shopping_fee_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+
   
 end
