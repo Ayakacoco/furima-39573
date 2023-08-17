91d4fee2ea633e062f2bd4a48db8979c03c507b0
Ayakacoco <ayakacoco28@gmail.com>
2023-08-17 21:11:59 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit]
+  before_action :contributor_confirmation, only: [:edit, :destroy]
   #before_action :move_to_index, except: [:index]
 
   def index
@@ -38,12 +38,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user == @item.user
       @item.destroy
-      redirect_to root_path(@item)
-    else
-      redirect_to root_path(@item)
-    end
+      redirect_to root_path
   end
 
   private
