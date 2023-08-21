4c45f8b0dcae6621e8df9828a1c72323c42410fd
Ayakacoco <ayakacoco28@gmail.com>
2023-08-21 14:16:25 +0900

    クレジットカード再入力

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,10 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!, only: :index
-  before_action :move_to_index, only: [:index]
+  before_action :move_to_index, only: :index
 
 
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
@@ -24,6 +23,7 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
   end
 
   def order_params
